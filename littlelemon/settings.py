6015835b5fd873de7b8e6d440204dd1ec269a2db
Sorbent <sorbent@gmail.com>
2025-02-17 22:51:22 +0000

    Second commit - settings updates

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservations',
+    'reservation',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
