93b7f58abd30c0d251ddfd244104ca68b64acb67
Sorbent <sorbent@gmail.com>
2025-02-18 22:25:03 +0000

    Add views, serializers and implement Booking and menu Item details

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from reservation import views
+
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('reservation.urls')),
+    path('restaurant/booking/', include(router.urls)),
+    
 ]
