58c7cc5cd9f9232a74547dad2faf1066d5cf0d1f
Sorbent <sorbent@gmail.com>
2025-02-20 00:11:51 +0000

    Add tests

--- reservation/models.py
+++ reservation/models.py
@@ -7,7 +7,7 @@ class Menu(models.Model):
     inventory = models.IntegerField(null=False) 
 
     def __str__(self): 
-        return self.title
+        return f'{self.title} : {str(self.price)}'
 
 class Booking(models.Model):
     name = models.CharField(max_length=255) 
--- reservation/urls.py
+++ reservation/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [ 
     path('sayHello/', views.sayHello, name='sayHello'), 
     path('', views.index, name='index'),
-    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/', views.MenuItemsView.as_view(), name='menu-list'),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token),
 ]
